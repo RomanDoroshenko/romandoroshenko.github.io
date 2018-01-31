3a2cf4eed1c56ef9d4a1075f8139c381cdca477c
Roman Doroshenko <roman.doroshenko.tlt@gmail.com>
2018-01-31 08:07:42 +0000

    bottom menu

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
 'use strict';
 importScripts('sw-toolbox.js');
-toolbox.precache(["index.html","styles.css","bundle.js"]);
+toolbox.precache([
+    "index.html",
+    "styles.css"
+    'react-md-styles.css'
+    "bundle.js"
+]);
 toolbox.router.get('/assets/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
